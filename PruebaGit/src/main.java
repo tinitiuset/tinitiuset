016d8ecb12a2e0e744075646c8feb323f3469c82
TINI <TINI@Portatil>
2020-05-05 09:53:11 +0200

    commit2

--- PruebaGit/src/main.java
+++ PruebaGit/src/main.java
@@ -4,6 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo.");
+		System.out.println("Push a Github");
 	}
 
 }
